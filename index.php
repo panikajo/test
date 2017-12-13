078e03a3381f050d4984d29009dd9557817d6b4f
panikajo <panikajo@gmail.com>
2017-12-13 08:42:05 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include 'header.php';
+include 'order.php';
+include 'footer.php';
+include 'info.php';
+include 'cat.php';
+include 'corf.php';
+ ?>
---
+++ order.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Страница товара</title>
+  </head>
+  <body>
+<?php
+echo 'Index shop';
+?>  </body>
+</html>
